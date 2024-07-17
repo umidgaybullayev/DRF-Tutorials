0654dc5696b551556f6534cc8fc09326a76e812d
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-17 14:44:29 +0500

    done

--- DRF-React-Todo/backend/todos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class TodoSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Todos
-        fields = '__all__'
--- DRF-React-Todo/backend/todos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ListTodos, DetailTodo
-
-urlpatterns = [
-    path('<int:pk>', DetailTodo.as_view()),
-    path('', ListTodos.as_view())
-]
