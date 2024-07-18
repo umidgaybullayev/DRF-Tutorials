29cb219ca1a283a43b0fb3b0013361abd7fb5a0c
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-18 16:34:54 +0500

    feat: function view

--- 3-dars/books/views.py
+++ 3-dars/books/views.py
@@ -1,9 +1,17 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 class BookListApiView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
