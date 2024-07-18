1dc1fe0f16ee80e3ebee517f8b10d80eec9a4a2c
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-18 17:06:57 +0500

    feat: connect BookDeleteAPIView and BookUpdateAPIView to url

--- 3-dars/books/urls.py
+++ 3-dars/books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BookListApiView, book_list_view, BookDetailView
+from .views import BookListApiView, book_list_view, BookDetailAPIView, BookDeleteAPIView, BookUpdateAPIView
 
 urlpatterns = [
     path('', BookListApiView.as_view(), ),
-    path('<int:pk>/', BookDetailView.as_view()),
+    path('<int:pk>/', BookDetailAPIView.as_view()),
+    path('<int:pk>/delete/', BookDeleteAPIView.as_view()),
+    path('<int:pk>/update/', BookUpdateAPIView.as_view()),
     path('books/', book_list_view),
 ]
