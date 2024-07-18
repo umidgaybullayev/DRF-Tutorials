549ae9c02648e5d4d213d557583b6455eb6c2ea3
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-18 16:50:23 +0500

    feat: connect BookDetailView to url

--- 3-dars/books/urls.py
+++ 3-dars/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListApiView, book_list_view
+from .views import BookListApiView, book_list_view, BookDetailView
 
 urlpatterns = [
     path('', BookListApiView.as_view(), ),
-    path('books/', book_list_view)
+    path('<int:pk>/', BookDetailView.as_view()),
+    path('books/', book_list_view),
 ]
