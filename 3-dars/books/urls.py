9b548e11113cd0310aaf8f9ecbf5b25527c561d3
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-18 16:36:44 +0500

    feat: function view connect url

--- 3-dars/books/urls.py
+++ 3-dars/books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListApiView
+from .views import BookListApiView, book_list_view
 
 urlpatterns = [
-    path('', BookListApiView.as_view(), )
+    path('', BookListApiView.as_view(), ),
+    path('books/', book_list_view)
 ]
