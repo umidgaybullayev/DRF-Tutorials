d4eb2c09d67ef2d260f1a15805305bc83fb29860
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-19 16:23:21 +0500

    feat: connect BookListCreateApiView to url

--- 3-dars/books/urls.py
+++ 3-dars/books/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('books/', BookListApiView.as_view(), ),
+    path('book/', BookListCreateApiView.as_view()),
     path('books/create/', BookCreateApiView.as_view()),
     path('books/<int:pk>/', BookDetailAPIView.as_view()),
     path('books/<int:pk>/delete/', BookDeleteAPIView.as_view()),
