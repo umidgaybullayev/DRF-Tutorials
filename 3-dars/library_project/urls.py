df851624ce06e7d8cc6f15ad3baa351183c6832d
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-07-22 16:57:26 +0500

    feat: import staticfiles_urlpatterns

--- 3-dars/library_project/urls.py
+++ 3-dars/library_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -30,3 +31,5 @@ urlpatterns = [
         'redoc', cache_timeout=0), name='schema-redoc'),
             
 ]
+
+urlpatterns += staticfiles_urlpatterns()
